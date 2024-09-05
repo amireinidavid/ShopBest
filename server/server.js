f66fc91970ecf6487a9a949cf19c17ea45ea6cc1
amireinidavid <realheritagecompany@gmail.com>
2024-09-06 00:20:50 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://shop-best.vercel.app",
+    origin: "*",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
