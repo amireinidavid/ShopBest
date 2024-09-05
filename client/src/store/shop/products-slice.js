173fc9830149fa079eed2475b82ee58368e84930
amireinidavid <realheritagecompany@gmail.com>
2024-09-06 00:05:45 +0100

    Update products-slice.js

--- client/src/store/shop/products-slice.js
+++ client/src/store/shop/products-slice.js
@@ -18,7 +18,7 @@ export const fetchAllFliteredProducts = createAsyncThunk(
     });
 
     const result = await axios.get(
-      `http://localhost:5000/api/shop/products/get?${query}`
+      `https://shop-best-server.vercel.app/api/shop/products/get?${query}`
     );
 
     console.log(result);
@@ -31,7 +31,7 @@ export const fetchProductDetails = createAsyncThunk(
   "/products/fetchProductDetails",
   async (id) => {
     const result = await axios.get(
-      `http://localhost:5000/api/shop/products/get/${id}`
+      `https://shop-best-server.vercel.app/api/shop/products/get/${id}`
     );
 
     return result?.data;
